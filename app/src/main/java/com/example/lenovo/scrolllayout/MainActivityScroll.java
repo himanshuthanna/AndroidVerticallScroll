e0eb357e0e42f6b2813c333d85b8c29636c9b62e
himanshuthanna <31863402+himanshuthanna@users.noreply.github.com>
2017-10-28 11:24:58 +0530

    horizontal Scrolling

---
+++ app/src/main/java/com/example/lenovo/scrolllayout/MainActivityScroll.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.scrolllayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityScroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_scroll);
+    }
+}
